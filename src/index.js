b4d1f3ec6271f0257d984cf9783805f014546a5a
coazit <coazit@gmail.com>
2022-01-17 20:38:58 -0500

    addded auth5

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
