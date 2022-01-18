d7126a942168bc4f5b6e468146f94f31e3403ed1
coazit <coazit@gmail.com>
2022-01-17 23:18:29 -0500

    aut-mod-7

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now Auth!!!</h1>
+        <h1>We now have Auth!</h1>
       </header>
       <AmplifySignOut />
     </div>
   );
 }
+
 
 export default withAuthenticator(App);
