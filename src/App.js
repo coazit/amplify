568fa3206fb3e724e7dca48ff6a2702bda18bccb
coazit <coazit@gmail.com>
2022-01-17 20:23:41 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
+        <h1>We now have Auth!!!</h1>
       </header>
       <AmplifySignOut />
     </div>
